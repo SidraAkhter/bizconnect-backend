f9179431c0fa491f6b88971e037e59e2d81ece40
SidraAkhter <sidraakhter16@gmail.com>
2025-08-01 18:03:53 +0500

    initinal commit

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [config.FRONTEND_ORIGIN, "http://localhost:5000"],
+    origin: [config.FRONTEND_ORIGIN, "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie", "X-Requested-With"],
